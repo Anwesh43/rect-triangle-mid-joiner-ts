e2448e558c5756cada176c5d58d4835a6f266968
Anwesh43 <anweshthecool0@gmail.com>
2020-10-29 21:41:08 +0530

    adding State class

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const parts : number = 4 
+const scGap : number = 0.02 / parts 
 const strokeFactor : number = 90 
 const rwFactor : number = 8 
 const hwFactor : number = 12 
@@ -100,5 +101,29 @@ class Stage {
         stage.initCanvas()
         stage.render()
         stage.handleTap()
+    }
+}
+
+class State {
+
+    scale : number = 0 
+    dir : number = 0 
+    prevScale : number = 0 
+
+    update(cb : Function) {
+        this.scale += scGap * this.dir 
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir 
+            this.dir = 0 
+            this.prevScale = this.scale 
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
     }
 }
